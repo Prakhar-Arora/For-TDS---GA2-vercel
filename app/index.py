41abbcbf61e84715ce9328563b3ad9bdd698489a
Prakhar-Arora <shri.prakhar.arora@gmail.com>
2025-10-02 02:15:34 +0530

    Resolved credentials issue.

--- app/index.py
+++ app/index.py
@@ -16,9 +16,7 @@ origins = [
 # Enable CORS for POST from any origin
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
